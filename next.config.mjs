4dffbb3a199be9850cea821f3058b523561eeed7
allbutone <allbutone@foxmail.com>
2026-04-08 17:08:39 +0800

    feat: 在 server component 中 async load cabins

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,23 @@ const nextConfig = {
   /* config options here */
   images: {
     qualities: [25, 50, 75, 100],
-  }
+    remotePatterns: [
+      // public url
+      {
+        protocol: "https",
+        hostname: "zzudlfaityyrmtxwajxy.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+      // signed url
+      {
+        protocol: "https",
+        hostname: "zzudlfaityyrmtxwajxy.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/sign/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
