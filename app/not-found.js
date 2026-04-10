5c9de0fdbda6b6bf7fdb1b951f72bef38c688edf
allbutone <allbutone@foxmail.com>
2026-04-10 11:03:21 +0800

    feat: 提供 not-found.js (not found file), 当访问不存在的 route 时, 会默认渲染该文件

---
+++ app/not-found.js
@@ -0,0 +1,19 @@
+function NotFound(props) {
+  console.log(`props`, props);// 结构同样是 {params, searchParams}
+  return (
+    <main className='text-center space-y-6 mt-4'>
+      <h1 className='text-3xl font-semibold'>
+        This page could not be found :(
+      </h1>
+      <a
+        href='/'
+        className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg'
+      >
+        Go back home
+      </a>
+    </main>
+  );
+}
+
+export default NotFound;
+
