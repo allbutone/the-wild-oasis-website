a880593b4a9f8d2ca63e2d1ac629045c9ec0537a
allbutone <allbutone@foxmail.com>
2026-04-21 11:25:10 +0800

    feat: 当 click FilterByCapacity 下 button 时, 为 button 添加 active background color

--- app/cabins/FilterByCapacity.js
+++ app/cabins/FilterByCapacity.js
@@ -5,6 +5,9 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 export default function FilterByCapacity() {
   // read current search params
   const searchParams = useSearchParams();
+
+  const currentCapacityType = searchParams.get('capacityType') ?? 'all';
+
   // 注意: nextjs 有两个 useRouter, 在 next/navigation 下的才是 app router, 另一个是 page router
   const router = useRouter();
   // read current URI
@@ -21,17 +24,17 @@ export default function FilterByCapacity() {
   }
   return (
     <div className="flex border border-primary-800 ">
-      <Button onClick={() => handleClick("all")}>All cabins</Button>
-      <Button onClick={() => handleClick("small")}>1&mdash;3</Button>
-      <Button onClick={() => handleClick("medium")}>4&mdash;8</Button>
-      <Button onClick={() => handleClick("large")}>9&mdash;12</Button>
+      <Button onClick={() => handleClick("all")} isActive={currentCapacityType === 'all'}>All cabins</Button>
+      <Button onClick={() => handleClick("small")} isActive={currentCapacityType === 'small'}>1&mdash;3</Button>
+      <Button onClick={() => handleClick("medium")} isActive={currentCapacityType === 'medium'}>4&mdash;8</Button>
+      <Button onClick={() => handleClick("large")} isActive={currentCapacityType === 'large'}>9&mdash;12</Button>
     </div>
   );
 }
 
-export function Button({ children, onClick }) {
+export function Button({ children, onClick, isActive }) {
   return (
-    <button className="px-5 py-2 hover:bg-primary-700" onClick={onClick}>
+    <button className={ `px-5 py-2 hover:bg-primary-700 ${isActive ? 'bg-primary-700' : ''}` } onClick={onClick}>
       {children}
     </button>
   );
